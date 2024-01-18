a8803ea2bc0acb0a879f47bbb9e8519989915ceb
Francis <k1k1dipsen@yahoo.fr>
2024-01-18 13:27:54 +0100

    modify port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cookie());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 
-const port = 8000;
+const port = 3306;
 
 require("./database");
 
